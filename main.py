4a139b1f88529438a36cb07fd6a59a8b7413e3cc
diyari1996 <dyari.ameen@gmail.com>
2021-06-27 10:17:55 +0300

    hahaha

--- main.py
+++ main.py
@@ -4,17 +4,17 @@ Created on Mon May 31 14:12:00 2021
 
 @author: Dyari M.shareef
 """
-
 import pandas as pd
 import librosa
 import numpy as np
 
-annotatedData=pd.read_csv("train_test_split.csv")
-Xtrain=pd.concat((annotatedData['train_cat'], annotatedData['train_dog']),axis=0).dropna()
-ytrain=Xtrain.str.split('_').str[0]
+annotatedData=pd.read_csv("meta-data.csv")
 
-Xtest=pd.concat((annotatedData['test_cat'], annotatedData['test_dog']),axis=0).dropna()
-ytest=Xtest.str.split('_').str[0]
+Xtrain=annotatedData['filename']
+ytrain=annotatedData['category']
+
+Xtest=annotatedData['filename-test'].dropna()
+ytest=annotatedData['category-test'].dropna()
 
 
 def features_extractor(file):
@@ -25,24 +25,21 @@ def features_extractor(file):
 
 
 def MFCCExctracting(arg):
-    ### Now we iterate through every audio file and extract features 
-    ### using Mel-Frequency Cepstral Coefficients
     extracted_features=[]
     for row in arg:
-        featrs=features_extractor('cats_dogs\\'+row)
+        featrs=features_extractor('audio\\'+row)
         extracted_features.append(featrs)
-    
-    #extracted_features_df=pd.DataFrame(extracted_features,columns=['feature'])
     return extracted_features
 
 Xtrain=MFCCExctracting(Xtrain)
 Xtrain=np.array(Xtrain)
+ytrain=np.array(ytrain)
+
 Xtest=MFCCExctracting(Xtest)
 Xtest=np.array(Xtest)
+ytest=np.array(ytest)
 
-### Label Encoding
-###y=np.array(pd.get_dummies(y))
-### Label Encoder
+
 from tensorflow.keras.utils import to_categorical
 from sklearn.preprocessing import LabelEncoder
 labelencoder=LabelEncoder()
@@ -56,16 +53,26 @@ from tensorflow.keras.optimizers import Adam
 from sklearn import metrics
 
 ### No of classes
-num_labels=ytrain.shape[1]
+num_labels=50
 
 model=Sequential()
 ###first layer
 model.add(Dense(100,input_shape=(40,)))
-model.add(Activation('relu'))
-
+model.add(Activation('sigmoid'))
+model.add(Dropout(0.5))
 ###second layer
-model.add(Dense(50))
-model.add(Activation('relu'))
+model.add(Dense(200))
+model.add(Activation('sigmoid'))
+model.add(Dropout(0.5))
+###third layer
+model.add(Dense(200))
+model.add(Activation('sigmoid'))
+model.add(Dropout(0.5))
+
+###fourth layer
+model.add(Dense(100))
+model.add(Activation('sigmoid'))
+model.add(Dropout(0.5))
 
 ###final layer
 model.add(Dense(num_labels))
@@ -75,16 +82,19 @@ model.summary()
 
 model.compile(loss='categorical_crossentropy',metrics=['accuracy'],optimizer='adam')
 
+
 ## Trianing my model
 from tensorflow.keras.callbacks import ModelCheckpoint
 from datetime import datetime 
 
-num_epochs =15
-num_batch_size = 32
 
 checkpointer = ModelCheckpoint(filepath='audio_classification.hdf5', verbose=1, save_best_only=True)
+
 start = datetime.now()
-model.fit(Xtrain, ytrain, batch_size=num_batch_size, epochs=num_epochs, validation_data=(Xtest, ytest), callbacks=[checkpointer], verbose=1)
+
+model.fit(Xtrain, ytrain, validation_data=(Xtest, ytest), batch_size=10, epochs=25000, callbacks=[checkpointer], verbose=1)
+
+
 duration = datetime.now() - start
 print("Training completed in time: ", duration)
 
@@ -106,6 +116,19 @@ mfccs_scaled_features=mfccs_scaled_features.reshape(1,-1)
 predicted_label=model.predict_classes(mfccs_scaled_features)
 prediction_class = labelencoder.inverse_transform(predicted_label) 
 prediction_class[0]
+
+
+# epochsUsed=[]
+# accuracyUsed=[]
+# for i in range(1000):
+#     model.fit(Xtrain, ytrain, batch_size=num_batch_size, epochs=i, validation_data=(Xtest, ytest), callbacks=[checkpointer], verbose=1)
+#     test_accuracy=model.evaluate(Xtest,ytest,verbose=0)
+#     accuracyUsed.append(test_accuracy[1])
+
+# import matplotlib.pyplot as plt
+
+# plt.plot(range(401),accuracyUsed)
+
 
 # for row in Xtrain:
 #     print(librosa.get_duration(filename=('cats_dogs\\'+row)))
